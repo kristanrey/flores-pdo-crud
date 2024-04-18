fecf7668788ef390dccfe5ff7c4d8d216a8f8599
kristanrey <kristanreyflores08@gmail.com>
2024-04-18 15:39:55 +0800

    revised code

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 <?php
-// Database credentials
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'tan');
-
-// Attempt to connect to the database
-try {
+ 
+/* Attempt to connect to MySQL database */
+try{
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    // If connection fails, display an error message and exit
+} catch(PDOException $e){
     die("ERROR: Could not connect. " . $e->getMessage());
 }
 ?>
